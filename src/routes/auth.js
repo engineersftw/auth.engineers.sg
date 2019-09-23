1c49eb1e3fb6bc4dadad9affeef5d89d36004fb2
Michael Cheng <mcheng.work@gmail.com>
2019-09-23 20:57:20 +0800

    Add success screen.

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,22 +3,39 @@ const router = express.Router()
 const jwt = require('jsonwebtoken')
 const passport = require('passport')
 
-router.post('/login', function (req, res, next) {
-  passport.authenticate('local', { session: false }, (err, user, info) => {
-    if (err || !user) {
-      return res.status(400).json({
-        message: 'Something is not right',
-        user: user
-      })
+function renderSuccess(res, user, token) {
+  return res.set({
+    'X-JWT-TOKEN': token
+  }).render('success', {
+    title: 'Engineers.SG - Successful Login',
+    returnUrl: 'https://engineers.sg',
+    user: user,
+    token: token
+  })
+}
+
+function renderError(res, message, code = 401) {
+  return res.status(code).render('index', {
+    title: 'Engineers.SG - Login Failure',
+    errMessage: message
+  })
+}
+
+function loginCallback(res, req, err, user) {
+  if (err || !user) {
+    return renderError(res, 'Please check your login credentials.')
+  }
+  req.login(user, { session: false }, (err) => {
+    if (err) {
+      return renderError(res, err)
     }
-    req.login(user, { session: false }, (err) => {
-      if (err) {
-        res.send(err)
-      }
-      const token = jwt.sign(user, process.env.JWT_SECRET)
-      return res.json({ user, token })
-    })
-  })(req, res)
+    const token = jwt.sign(user, process.env.JWT_SECRET)
+    return renderSuccess(res, user, token)
+  })
+}
+
+router.post('/login', function (req, res, next) {
+  passport.authenticate('local', { session: false }, (err, user) => loginCallback(res, req, err, user))(req, res)
 })
 
 router.get('/github', function (req, res, next) {
@@ -26,41 +43,13 @@ router.get('/github', function (req, res, next) {
 })
 
 router.get('/github/callback', function (req, res, next) {
-  passport.authenticate('github', { session: false }, (err, user) => {
-    if (err || !user) {
-      return res.status(400).json({
-        message: 'Something is not right',
-        user: user
-      })
-    }
-    req.login(user, { session: false }, (err) => {
-      if (err) {
-        res.send(err)
-      }
-      const token = jwt.sign(user, process.env.JWT_SECRET)
-      return res.json({ user, token })
-    })
-  })(req, res)
+  passport.authenticate('github', { session: false }, (err, user) => loginCallback(res, req, err, user))(req, res)
 })
 
 router.get('/twitter', passport.authenticate('twitter'))
 
 router.get('/twitter/callback', function (req, res, next) {
-  passport.authenticate('twitter', { session: false }, (err, user) => {
-    if (err || !user) {
-      return res.status(400).json({
-        message: 'Something is not right',
-        user: user
-      })
-    }
-    req.login(user, { session: false }, (err) => {
-      if (err) {
-        res.send(err)
-      }
-      const token = jwt.sign(user, process.env.JWT_SECRET)
-      return res.json({ user, token })
-    })
-  })(req, res)
+  passport.authenticate('twitter', { session: false }, (err, user) => loginCallback(res, req, err, user))(req, res)
 })
 
 module.exports = router
