389d345211cc2dc9fce888ab4db5f34069fad679
Michael Cheng <mcheng.work@gmail.com>
2019-09-26 22:11:09 +0800

    Use Redis as the session store

--- src/app.js
+++ src/app.js
@@ -6,6 +6,13 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
 
+const redis = require('redis')
+const session = require('express-session')
+const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379'
+
+let RedisStore = require('connect-redis')(session)
+let redisclient = redis.createClient(REDIS_URL)
+
 const indexRouter = require('./routes/index')
 const authRouter = require('./routes/auth')
 const usersRouter = require('./routes/users')
@@ -23,7 +30,14 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(require('express-session')({ secret: process.env.EXPRESS_SESSION_SECRET, resave: true, saveUninitialized: true }))
+app.use(
+  session({
+    store: new RedisStore({ client: redisclient }),
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+)
 
 app.use('/', indexRouter)
 app.use('/auth', authRouter)
