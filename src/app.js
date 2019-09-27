19892e54171bf9d117fe3566c1a4b53717c049a6
Michael Cheng <mcheng.work@gmail.com>
2019-09-27 09:38:09 +0800

    Allow debugging in tests.

--- src/app.js
+++ src/app.js
@@ -5,13 +5,7 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
-
-const redis = require('redis')
 const session = require('express-session')
-const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379'
-
-let RedisStore = require('connect-redis')(session)
-let redisclient = redis.createClient(REDIS_URL)
 
 const indexRouter = require('./routes/index')
 const authRouter = require('./routes/auth')
@@ -30,14 +24,31 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(
-  session({
-    store: new RedisStore({ client: redisclient }),
-    secret: process.env.EXPRESS_SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true
-  })
-)
+
+if (process.env.NODE_ENV === 'test') {
+  app.use(
+    session({
+      secret: process.env.EXPRESS_SESSION_SECRET,
+      resave: true,
+      saveUninitialized: true
+    })
+  )
+} else {
+  const redis = require('redis')
+  const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379'
+
+  let RedisStore = require('connect-redis')(session)
+  let redisclient = redis.createClient(REDIS_URL)
+
+  app.use(
+    session({
+      store: new RedisStore({ client: redisclient }),
+      secret: process.env.EXPRESS_SESSION_SECRET,
+      resave: false,
+      saveUninitialized: true
+    })
+  )
+}
 
 app.use('/', indexRouter)
 app.use('/auth', authRouter)
